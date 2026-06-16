eabf0ce33b552e33b6a76a994a2183bd2acc949a
henrybbonisson-tech <henry.bbonisson@aluno.edu.es.gov.br>
2026-06-16 12:01:17 -0300

    Create index.php

---
+++ aula 01 - php/index.php
@@ -0,0 +1,17 @@
+<!DOCKTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula PHP</title>
+    </head>
+    <body>
+
+            <?php
+                echo <h1>Óla,<br> Mundo</h1>;
+            ?>
+
+    </body>
+
+
+
+</html>
